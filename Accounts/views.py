4f63bf11ff6ed691e0f81a52c9c4536fe6654a71
muhammedbadsha <badshanasar123@gmail.com>
2023-09-30 11:39:46 +0530

    jwt token added

--- Accounts/views.py
+++ Accounts/views.py
@@ -21,6 +21,15 @@ import uuid
 
 # Create your views here.
 
+"""JWT Tockens manually creates"""
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+
 """send email notification """
 def email_sending_otp(email,otp):
     subject = "BLOGGER HUB Email verification "
